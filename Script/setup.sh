5b8cca2ce954cbafa6ac7265847e830cea400d80
alanpham2k2 <110401923+alanpham2k2@users.noreply.github.com>
2025-12-18 16:20:51 +0700

    Simplify Docker installation in setup script
    Removed manual Docker installation steps and replaced with a single curl command for simplicity.

--- Script/setup.sh
+++ Script/setup.sh
@@ -22,22 +22,12 @@ sudo apt-get install -y \
     tar \
     less \
     groff \
-    gnupg \
     bash-completion
 
 
 echo "Installing Docker & Docker Compose..."
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg --yes
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+curl -fsSL https://get.docker.com | sh
 sudo systemctl enable --now docker
-sudo groupadd docker || true
 sudo usermod -aG docker $USER
 echo "Docker successfully installed!"
 
